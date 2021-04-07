6287bfbde902271524e8271acc762224a86bd35b
Prithwee Das <prithweedas123@gmail.com>
2021-04-07 19:16:45 +0530

    feat(db): consuming from shared types package

--- packages/db/src/mongo/index.ts
+++ packages/db/src/mongo/index.ts
@@ -1,2 +1,3 @@
 export { db } from './client'
 export * as counter from './models/counter'
+// export * as account from './models/account'
---
+++ packages/db/src/mongo/models/account.ts
@@ -0,0 +1,7 @@
+import { Account } from '@banking/types'
+
+const account: Account = {
+  address: '',
+  pan: '',
+  username: ''
+}
